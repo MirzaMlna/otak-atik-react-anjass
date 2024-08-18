68e0fac87b9aa32b15d655708c506f5b94b1b852
MirzaMlna <mirzamaulana713@gmail.com>
2024-08-19 01:30:37 +0800

    Belajar Motion Framer Anjay

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,17 +1,38 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
 import PropTypes from "prop-types";
+import { Button } from "react-bootstrap";
 
 HeroSection.propTypes = {
   lightTheme: PropTypes.bool,
 };
 
 export default function HeroSection({ lightTheme }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const visibleHandler = () => setIsVisible(!isVisible);
+
   return (
     <div
       className={`d-flex justify-content-center align-items-center vh-100 ${
         lightTheme ? "bg-light text-dark" : "bg-dark text-light"
       }`}
     >
-      <h1>HeroSection</h1>
+      <div className="text-center">
+        <motion.div
+          animate={{ opacity: isVisible ? 1 : 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <h1>HeroSection</h1>
+        </motion.div>
+
+        <Button
+          className="mt-3"
+          variant={lightTheme ? "dark" : "light"}
+          onClick={visibleHandler}
+        >
+          {isVisible ? "Hide Title" : "Show Title"}
+        </Button>
+      </div>
     </div>
   );
 }
